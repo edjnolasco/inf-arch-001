1b571e56e722e33ff1cefb584f620e54fb174c0e
edjnolasco <edjnolasco@gmail.com>
2026-04-12 22:13:21 -0400

    feat: project-base archivos domain e infrastructure incorporados

---
+++ INF.5120.Arch001/INF.5120.Arch001.Domain/Entities/Country.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF._5120.Arch001.Domain.Entities
+{
+    public class Country
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public required string Description { get; set; }
+
+        [Required]
+        public int IsoNum { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public required string IsoA2 { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public required string IsoA3 { get; set; }
+
+        [Required]
+        public bool IsEnable { get; set; } = true;
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
